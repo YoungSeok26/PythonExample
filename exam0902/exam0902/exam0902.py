6159b7a1d686134ea211b6c78d4324838cbc5c3c
YoungSeok26 <clown26@naver.com>
2015-09-02 13:46:10 +0900

    String Example Python
    문자열 예제 주석처리 문자열 함수 등등

---
+++ exam0902/exam0902/exam0902.py
@@ -0,0 +1,52 @@
+﻿#salary = int(input("Please enter your salary : "))
+#bonus = int(input("Please enter your bonus : "))
+
+#payCheck = salary+bonus
+##주석처리
+##payCheck =  float(salary)+float(bounus)
+
+#'''
+#여러줄 주석 처리하는 방법
+#비쥬얼 주석 처리하는 단축키 사용가능 (Ctrl + k + c)
+#'''
+
+
+#"""
+#이거는 여러줄 출력할 때도 사용
+#"""
+
+#print(payCheck)
+
+##한글저장은 고급옵션에서 UTF-8로 변환
+#print("안녕하십니까?")
+
+##타입체크하는 방법
+#print(type(payCheck))
+
+#print("="*20)
+
+#print("salary  "  + " and bonus  ")
+
+#name = "greenjoa"
+
+#print(name[0])
+#print(name[-1])
+
+
+#info = "201111272YoungSeok"
+#sid = info[:9]
+#sname = info[9:]
+#print(sid+" "+sname)
+
+
+#a= "I eat %10s apples."% "five"
+#ㅁ= "I eat %-10s apples."% "five"
+
+#"{0:<10}".fomat("hi")
+
+#print(a)
+#print(ㅁ)
+
+#exit = input("종료 하시겠습니까??? ")
+
+#print(exit.swapcase())
